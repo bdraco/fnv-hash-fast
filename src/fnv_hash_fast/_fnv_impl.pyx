fc608b7a1f1fee03eab2598d25f808249ea0de95
J. Nick Koston <nick@koston.org>
2023-04-03 20:28:08 -1000

    feat: init version (#3)

--- src/fnv_hash_fast/_fnv_impl.pyx
+++ src/fnv_hash_fast/_fnv_impl.pyx
@@ -7,5 +7,5 @@ import cython
 cdef extern from "fnv_wrapper.h":
     int _cpp_fnv1a_32(string data)
 
-def _fnv1a_32(ulid_str: str) -> int:
-    return _cpp_fnv1a_32(ulid_str.encode("ascii"))
+def _fnv1a_32(data: bytes) -> int:
+    return _cpp_fnv1a_32(data)
