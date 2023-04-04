8f12401b960d902649031b0c69e61f9bc5d3ecd7
J. Nick Koston <nick@koston.org>
2023-04-03 20:13:40 -1000

    fix: fixes

--- tests/test_init.py
+++ tests/test_init.py
@@ -2,4 +2,5 @@ from fnv_hash_fast import fnv1a_32
 
 
 def test_fnv1a_32():
-    assert fnv1a_32(b"hello") == 0x8C959D4E
+    assert fnv1a_32(b"hello") == 2358615374
+    assert fnv1a_32(b"goodbye") == 1335831723
