94d626d14abb5394d05d9a7e11367ce1b1b1cb0e
J. Nick Koston <nick@koston.org>
2023-04-03 20:39:51 -1000

    chore: add more test coverage

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,6 +1,18 @@
+from fnvhash import fnv1a_32 as fnvhash_fnv1a_32
+
 from fnv_hash_fast import fnv1a_32
 
 
 def test_fnv1a_32():
     assert fnv1a_32(b"hello") == 1335831723
     assert fnv1a_32(b"goodbye") == 1188507472
+    assert fnv1a_32(b"goodbye" * 4096) == 386067909
+
+
+def test_fnvhash_fnv1a_32():
+    for test_data in (
+        b"hello",
+        b"goodbye",
+        b"goodbye" * 4096,
+    ):
+        assert fnv1a_32(test_data) == fnvhash_fnv1a_32(test_data)
